bae381e8aec0dd1928b947247ccde2fea0bc7348
Ssubhams19 <subhamsahoo02195@gmail.com>
2026-05-30 14:44:18 +0530

    Initialize temperature to 25 and add setter method

--- q2.kt
+++ q2.kt
@@ -1,4 +1,14 @@
 class Thermostat {
-    var temperature: Double = 0.0
+    var temperature: Double = 25.0
         private set
+
+    fun setTemperature(value: Double) {
+        temperature = value
+    }
+}
+
+fun main() {
+    val t = Thermostat()
+    t.setTemperature(30.0)
+    println(t.temperature)
 }
