cde0ad25d7c80140ef1bf3a668e05d0c90cdd01b
Ssubhams19 <subhamsahoo02195@gmail.com>
2026-05-30 14:43:15 +0530

    create q1.kt
    Ensure that deposit only adds positive amounts to balance.

--- q1.kt
+++ q1.kt
@@ -2,6 +2,18 @@ class BankAccount {
     private var balance: Double = 0.0
 
     fun deposit(amount: Double) {
-        balance += amount
+        if (amount > 0) {
+            balance += amount
+        }
     }
+
+    fun showBalance() {
+        println("Balance = $balance")
+    }
+}
+
+fun main() {
+    val account = BankAccount()
+    account.deposit(500.0)
+    account.showBalance()
 }
